ffa1b770ef5cb021ea6ab0836ba91e534c722949
Mauro Agnoletti <mauro.agnoletti@gmail.com>
2014-07-31 15:31:53 -0300

    ThinkUp.SignalR NuGet package added

--- Server/C#/ThinkUp.SignalR/Properties/AssemblyInfo.cs
+++ Server/C#/ThinkUp.SignalR/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ThinkUp.SignalR")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Think Up SignalR")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Think Up Studios")]
 [assembly: AssemblyProduct("ThinkUp.SignalR")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
